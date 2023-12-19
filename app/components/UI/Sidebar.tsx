78f7d3bff45896cca95bc3fddeb595f3dcd0d210
John Dao <104119657+johnd959@users.noreply.github.com>
2023-12-18 23:22:52 -0700

    Added signout to sidebar

--- app/components/UI/Sidebar.tsx
+++ app/components/UI/Sidebar.tsx
@@ -4,6 +4,8 @@ import { ReactElement, useState } from "react";
 import { Folder } from "@/app/Types/Folder";
 import { Group } from "@/app/Types/Group";
 import Folders from "./Folders";
+import { VscSignOut } from "react-icons/vsc";
+import { useUserAuth } from "@/app/_utils/auth-context";
 
  type Props = {
     foldersPackage: {folders:Folder[], setFolders: Function, handleDeleteFolder: Function},
@@ -18,13 +20,18 @@ import Folders from "./Folders";
 export default function Sidebar({foldersPackage, groupsPackage, filterByFolder}:Props){
 
     const [selectedTab, setSelectedTab]:[Boolean, Function] = useState(true);
+    const {firebaseSignOut} = useUserAuth();
 
 
     return(
         <div className="flex flex-row min-h-full bg-black text-base-content">
-        <ul className="w-12 bg-slate-800 flex flex-col gap-2 content-center pt-16">
-          <li onClick={() => setSelectedTab(true)} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out " + `${selectedTab? "bg-black":"bg-slate-800"}`}><VscFolder color="white" size={30}></VscFolder></li>
-          <li onClick={() => setSelectedTab(false)} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out " + `${!selectedTab? "bg-black":"bg-slate-800"}`}><FaUserGroup color="white" size={30}></FaUserGroup></li>
+        <ul className="w-12 bg-slate-800 flex flex-col justify-between gap-2 content-center pt-16">
+            <li>
+                         <span onClick={() => setSelectedTab(true)} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out " + `${selectedTab? "bg-black":"bg-slate-800"}`}><VscFolder color="white" size={30}></VscFolder></span>
+          <span onClick={() => setSelectedTab(false)} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out " + `${!selectedTab? "bg-black":"bg-slate-800"}`}><FaUserGroup color="white" size={30}></FaUserGroup></span> 
+            </li>
+            <li onClick={() => firebaseSignOut()} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out "}><VscSignOut color="white" size={30}></VscSignOut></li>
+
         </ul>
         <div className="w-60 text-white p-4">
             {selectedTab? <Folders filterByFolder={filterByFolder} folderList={foldersPackage.folders} setFolders={foldersPackage.setFolders} handleDeleteFolder={foldersPackage.handleDeleteFolder}  /> : <h2 className="text-lg">Groups</h2>}
