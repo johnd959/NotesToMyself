8c0f76ee9bc2061c0d739920d0bc0c8aceb05154
John Dao <104119657+johnd959@users.noreply.github.com>
2023-12-19 12:16:14 -0700

    Added some padding to sidebar

--- app/components/UI/Sidebar.tsx
+++ app/components/UI/Sidebar.tsx
@@ -24,7 +24,7 @@ export default function Sidebar({foldersPackage, groupsPackage, filterByFolder}:
 
 
     return(
-        <div className="flex flex-row min-h-full bg-black text-base-content">
+        <div className="flex pb-5 flex-row min-h-full bg-black text-base-content">
         <ul className="w-12 bg-slate-800 flex flex-col justify-between gap-2 content-center pt-16">
             <li>
                          <span onClick={() => setSelectedTab(true)} className={"cursor-pointer flex flex-row justify-center py-2 hover:bg-black transition-colors duration-400 ease-in-out " + `${selectedTab? "bg-black":"bg-slate-800"}`}><VscFolder color="white" size={30}></VscFolder></span>
